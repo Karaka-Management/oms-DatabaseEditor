bc7cdb848f354f24b717d3502f1c99cb6d98946f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['DatabaseEditor' => [
+    'CSV' => '',
+    'Database' => '',
+    'DatabaseType' => '',
+    'Excel' => '',
+    'Execute' => '',
+    'Export' => '',
+    'Host' => '',
+    'JSON' => '',
+    'Limit1000' => '',
+    'Login' => '',
+    'NoResults' => '',
+    'Password' => '',
+    'Port' => '',
+    'Query' => '',
+    'QueryResult' => '',
+    'Test' => '',
+    'Title' => '',
+    'Querys' => '',
+    'Creator' => '',
+    'Created' => '',
+]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\DatabaseEditor
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,20 +12,24 @@
 declare(strict_types=1);
 
 return ['DatabaseEditor' => [
-    'CSV'          => 'CSV',
-    'Database'     => 'Database',
+    'CSV' => 'CSV',
+    'Database' => 'Database',
     'DatabaseType' => 'Database Type',
-    'Excel'        => 'Excel',
-    'Execute'      => 'Execute',
-    'Export'       => 'Export',
-    'Host'         => 'Host',
-    'JSON'         => 'JSON',
-    'Limit1000'    => 'Limit 1000',
-    'Login'        => 'Login',
-    'NoResults'    => 'No Results',
-    'Password'     => 'Password',
-    'Port'         => 'Port',
-    'Query'        => 'Query',
-    'QueryResult'  => 'Query Result',
-    'Test'         => 'Test',
+    'Excel' => 'Excel',
+    'Execute' => 'Execute',
+    'Export' => 'Export',
+    'Host' => 'Host',
+    'JSON' => 'JSON',
+    'Limit1000' => 'Limit 1000',
+    'Login' => 'Login',
+    'NoResults' => 'No Results',
+    'Password' => 'Password',
+    'Port' => 'Port',
+    'Query' => 'Query',
+    'QueryResult' => 'Query Result',
+    'Test' => 'Test',
+    'Title' => '',
+    'Querys' => '',
+    'Creator' => '',
+    'Created' => '',
 ]];
