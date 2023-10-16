7fff869ce82e2901478031aab4d711c216b39bfa
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:38 +0000

    Add test cases for null models

--- tests/Models/NullQueryTest.php
+++ tests/Models/NullQueryTest.php
@@ -23,7 +23,7 @@ final class NullQueryTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\DatabaseEditor\Models\NullQuery
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,11 +32,21 @@ final class NullQueryTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\DatabaseEditor\Models\NullQuery
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
         $null = new NullQuery(2);
         self::assertEquals(2, $null->id);
+    }
+
+    /**
+     * @covers Modules\DatabaseEditor\Models\NullQuery
+     * @group module
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullQuery(2);
+        self::assertEquals(['id' => 2], $null);
     }
 }
