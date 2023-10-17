bfee0b803127618ae279f366a7f29662477f6b4f
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullQueryTest.php
+++ tests/Models/NullQueryTest.php
@@ -47,6 +47,6 @@ final class NullQueryTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullQuery(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
